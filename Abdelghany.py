ec7871b02a98ac5a0bc747d8a2cff08cbf70f091
Shayan Mousavi <shayanmousavi52@gmail.com>
2024-03-10 17:02:47 +0330

    k

--- Abdelghany.py
+++ Abdelghany.py
@@ -1,18 +1,17 @@
+from email.policy import default
 from pyomo.environ import *
 import openpyxl as x
+import pyomo.opt as pyo
 def optimize(stg_dat):    
-    
-    import pyomo.opt as pyo
-
     model = AbstractModel()
 
     # Sets rf import
     acwb = x.load_workbook("aircraft.xlsx")
     acws = acwb.active
     def R_init(model):
-        return [acws[i][0] for i in range(2, acws.max_row)]
+        return [acws[i][0].value for i in range(2, acws.max_row)]
     model.R = Set(initialize=R_init)
-    
+
     def F_init(model):
         return [stg_dat[i].flight_id for i in range(len(stg_dat))]
     model.F = Set(initialize=F_init)
@@ -22,41 +21,37 @@ def optimize(stg_dat):
     def c_init(model, r, f):
         for i in range(2, acws.max_row):
             for j in range(len(stg_dat)):
-                if (acws[i][8]==stg_dat[j].origin and acws[i][0]==r and stg_dat[j].flight_id==f):
-                    return acws[i][5]*(stg_dat[j].planned_arrival-stg_dat[j].planned_departure)/60
-                else:
-                    return 999999
+                if (acws[i][8].value==stg_dat[j].origin and acws[i][0].value==r and stg_dat[j].flight_id==f):
+                    return acws[i][5].value*(stg_dat[j].planned_arrival-stg_dat[j].planned_departure)/60
+        return 999999
     model.c = Param(model.R, model.F, initialize=c_init)
     
-    model.cd = Param(model.F, initialize=(0.75*120))
+    model.cd = Param(model.F, default=(0.75*120))
     
     def t_init(model, f):
         for i in range(len(stg_dat)):
             if stg_dat[i].flight_id == f:
                 return stg_dat[i].planned_departure
-            else:
-                return 99999
+        return 99999
     model.t = Param(model.F, initialize=t_init)
             
-    model.cc = Param(model.F, initialize=5000)
+    model.cc = Param(model.F, default=5000)
 
-    model.b = Param(model.R, model.F, initialize=1)
+    model.b = Param(model.R, model.F, default=1)
 
     def a_init(model, r, f):
         for i in range(2, acws.max_row):
             for j in range(len(stg_dat)):
-                if (acws[i][8]==stg_dat[j].origin and acws[i][0]==r and stg_dat[j].flight_id==f):
+                if (acws[i][8].value==stg_dat[j].origin and acws[i][0].value==r and stg_dat[j].flight_id==f):
                     return 0.5
-                else:
-                    return 99999
+        return 99999
     model.a = Param(model.R, model.F, initialize=a_init)
 
     def T_init(model, f):
         for j in range(len(stg_dat)):
             if stg_dat[j].flight_id==f :
                 return (stg_dat[j].planned_arrival-stg_dat[j].planned_departure)
-            else:
-                return 99999
+        return 99999
     model.T = Param(model.F, initialize=T_init)
 
     # Variables
@@ -74,7 +69,7 @@ def optimize(stg_dat):
     model.obj = Objective(rule=obj_expression,sense = minimize)
 
 
-    def C1(model, f, r):
+    def C1(model, r, f):
         return model.x[r,f] <= model.b[r,f]
 
     def C2(model, r):
@@ -83,7 +78,7 @@ def optimize(stg_dat):
     def C3(model, f):
         return sum(model.x[r, f] for r in model.R) == 1 - model.L[f]
 
-    def C4(model,f, r):
+    def C4(model,r, f):
         return model.m[f] >= model.x[r,f] * model.a[r,f]
 
     def C5(model, f):
@@ -94,10 +89,10 @@ def optimize(stg_dat):
 
 
 
-    model.Co1 = Constraint((model.F,model.R), rule=C1)
+    model.Co1 = Constraint((model.R,model.F), rule=C1)
     model.Co2 = Constraint(model.R, rule=C2)
     model.Co3 = Constraint(model.F, rule=C3)
-    model.Co4 = Constraint((model.F,model.R), rule=C4)
+    model.Co4 = Constraint((model.R,model.F), rule=C4)
     model.Co5 = Constraint(model.F, rule=C5)
     model.Co6 = Constraint(model.F, rule=C6)
 

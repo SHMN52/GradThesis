a437fe0b5c617961590cfb04620953c626651afb
Shayan Mousavi <shayanmousavi52@gmail.com>
2024-03-19 16:48:16 +0330

    k

--- main.py
+++ main.py
@@ -19,14 +19,14 @@ while current_stage <20:
     for i in op.F:
         for row1 in range(2, ws.max_row):
             if ws[row1][0].value==i:
-                ws[row1][7].value=value(op.m[i])
-                ws[row1][8].value=value(op.n[i])
-            for j in op.R:
-                if value(op.x[j,i])==1:
-                    for row2 in range(2, acws.max_row):
-                        if acws[row2][0].value==j:
-                            acws[row2][7].value=value(op.n[i])
-                            acws[row2][8].value=ws[row1][6].value
+                for j in op.R:
+                    if value(op.x[j,i])==1:
+                        for row2 in range(2, acws.max_row):
+                            if acws[row2][0].value==j:
+                                acws[row2][7].value=value(op.n[i])
+                                acws[row2][8].value=ws[row1][6].value
+                                ws[row1][7].value=value(op.m[i])
+                                ws[row1][8].value=value(op.n[i])
 
     loop_kpi=1
     for row in range(2, ws.max_row):

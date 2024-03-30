e617164bc110c8f19ebd232c0ac170484bd2cbb9
Shayan Mousavi <shayanmousavi52@gmail.com>
2024-03-30 17:22:23 +0330

    k

--- Abdelghany.py
+++ Abdelghany.py
@@ -43,7 +43,7 @@ def optimize(stg_dat,acws,apws):
 
     
     model.cc = Param(model.F, initialize = 50000)
-    model.UBn = Param(model.F, initialize = 1460)
+    
 
     def b_init(model, r, f):
         for i in range(2, acws.max_row+1):

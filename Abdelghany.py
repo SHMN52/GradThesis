df2ade5ed7cf00da8911c648d6d70f6b698f705b
Shayan Mousavi <shayanmousavi52@gmail.com>
2024-05-24 16:49:55 +0330

    k

--- Abdelghany.py
+++ Abdelghany.py
@@ -14,10 +14,11 @@ def optimize(stg_dat,acws,apws,current_stage):
     def F_init(model):
         return [stg_dat[i].flight_id for i in range(len(stg_dat))]
     model.F = Set(initialize=F_init) # Flight set
-
+    
     def I_init(model):
         return [apws[i][0].value for i in range(2, apws.max_row+1)]
     model.I = Set(initialize=I_init) # Origin set
+    
 
     
     # parameters
@@ -28,20 +29,20 @@ def optimize(stg_dat,acws,apws,current_stage):
             if stg_dat[j].flight_id == f :
                 return stg_dat[j].origin
     model.origin = Param(model.F, initialize = origin_init, within = Any) # Origin on flights
-
+    
     def Destination_init(model, f):
         for j in range(len(stg_dat)):
             if stg_dat[j].flight_id == f :
                 return stg_dat[j].dest
     model.dest = Param(model.F, initialize = Destination_init, within = Any) # Destination on flights
-
+    
 
     def a_init(model, r):
         for i in range(2, acws.max_row+1):
             if acws[i][0].value == r:
                 return acws[i][7].value + acws[i][6].value
     model.a = Param(model.R, initialize=a_init, within = NonNegativeIntegers) # Ready time of aircraft r (time of previus flight landing plus the turn-around time)
-
+    
     def T_init(model, f):
         for j in range(len(stg_dat)):
             if stg_dat[j].flight_id == f :
@@ -62,16 +63,17 @@ def optimize(stg_dat,acws,apws,current_stage):
             if stg_dat[i].flight_id == f :
                 return int(stg_dat[i].planned_departure + stg_dat[i].delay)
     model.t = Param(model.F, initialize=t_init, within = NonNegativeIntegers) # Scheduled departure time of flights (plus initial delay)
-
+    
     
     model.cc = Param(model.F, initialize = 50000, within = PositiveIntegers) # Estimated cost of flight cancellation
     
 
     def b_init(model,r, f, i, j):
-        for iter in range(2, acws.max_row+1):
-            if acws[iter][0].value == r :
-                if acws[iter][8].value == model.origin[f] and model.origin[f]==i and model.dest[f]==j:
-                    return 1
+        if model.origin[f]==i and model.dest[f]==j:
+            for iter in range(2, acws.max_row+1):
+                if acws[iter][0].value == r :
+                    if acws[iter][8].value == model.origin[f]:
+                        return 1
         return 0
     model.b = Param(model.R,model.F,model.I,model.I, initialize=b_init, within = Binary) # determines whether aircraft r can service flight f in current stage (same location and enought range of fly)
     
@@ -83,7 +85,7 @@ def optimize(stg_dat,acws,apws,current_stage):
                         return apws[i][4*j-3].value
         return 0
     model.depCap = Param(model.I, initialize=depCap_init)
-
+    
     def arCap_init(model, ap):
         for i in range(2, apws.max_row+1):
             if apws[i][0].value == ap :
@@ -104,7 +106,6 @@ def optimize(stg_dat,acws,apws,current_stage):
     model.n = Var(model.F, domain=NonNegativeIntegers) # Arrival variable for flight f
     
     
-    
     def obj_expression(model):
         return ( sum( model.c[r, f] * model.x[r,f,i,j] for r in model.R for f in model.F for i in model.I for j in model.I) 
                 +sum(model.cd[f] * (model.m[f]-model.t[f]) for f in model.F)
@@ -112,40 +113,33 @@ def optimize(stg_dat,acws,apws,current_stage):
 
     model.obj = Objective(rule=obj_expression,sense = minimize)
 
-
     def C1(model,r, f, i, j ):
         return model.x[r,f,i,j] <= model.b[r,f,i,j] 
-
     # C1 Checks assignability 
 
     def C2(model, r):
         return sum(model.x[r,f,i,j] for f in model.F for i in model.I for j in model.I) <= 1
-
     # C2 states that in a single stage, an aircraft can only accompany 
 
     def C3(model, f):
         return sum(model.x[r,f,i,j] for r in model.R for i in model.I for j in model.I) == 1 - model.L[f]
-    
     # C3: A flight must be either assigned to an aircraft or be cancceled
 
     def C4(model, f):
         return model.m[f] >= sum(model.x[r,f,i,j]*model.a[r] for r in model.R for i in model.I for j in model.I)
-    
     # C4: if flight f is assined to aircraft r, it can depart only after its ready time
 
     def C5(model, f):
         return model.n[f] == model.m[f] + model.T[f]
-
     # C5: a flights arrival is its departure plus the time of the flight
 
     def C6(model, f):
         return model.m[f] >= model.t[f]
-    
     # C6: a flight can only depart after it initial schedualed takeoff
 
     def C7(model , i):
         return sum(model.x[r,f,i,j] for r in model.R for f in model.F for j in model.I) <= model.depCap[i]
-    
+
     def C8(model , j):
         return sum(model.x[r,f,i,j] for r in model.R for f in model.F for i in model.I) <= model.arCap[j]
 
@@ -162,7 +156,6 @@ def optimize(stg_dat,acws,apws,current_stage):
     
    
     instance = model.create_instance()
-    
-    pyo.SolverFactory('cplex').solve(instance,tee=True)
+    pyo.SolverFactory('cplex').solve(instance,report_timing=True,tee=True)
     return instance
     
